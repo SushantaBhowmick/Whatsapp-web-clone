54c61486668745c883c9afba9a096896e70c5356
Sushanta Bhowmick <bhosushanta922@gmail.com>
2024-02-29 22:00:52 +0530

    add socket url

--- client/src/context/AccountProvider.jsx
+++ client/src/context/AccountProvider.jsx
@@ -13,7 +13,7 @@ const AccountProvider=({children})=>{
     const socket = useRef()
 
     useEffect(()=>{
-        socket.current=io('ws://localhost:5000')
+        socket.current=io('https://whatsapp-web-clone-socket.onrender.com/')
     },[])
 
 
